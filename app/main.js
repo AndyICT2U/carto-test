cc945e0beb2b881d419b53947c2efa180e09c14c
AndyICT2U <Andy@ict2u.com>
2018-11-15 15:01:44 +0000

    First Draft

--- app/main.js
+++ app/main.js
@@ -1,1 +1,160 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+define(["require", "exports", "esri/Map", "esri/views/SceneView", "esri/layers/FeatureLayer", "esri/layers/VectorTileLayer", "esri/geometry", "esri/renderers/UniqueValueRenderer", "esri/symbols", "esri/layers/support/LabelClass", "esri/Basemap", "esri/request"], function (require, exports, Map_1, SceneView_1, FeatureLayer_1, VectorTileLayer_1, geometry_1, UniqueValueRenderer_1, symbols_1, LabelClass, Basemap_1, esriRequest) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    Map_1 = __importDefault(Map_1);
+    SceneView_1 = __importDefault(SceneView_1);
+    FeatureLayer_1 = __importDefault(FeatureLayer_1);
+    VectorTileLayer_1 = __importDefault(VectorTileLayer_1);
+    UniqueValueRenderer_1 = __importDefault(UniqueValueRenderer_1);
+    Basemap_1 = __importDefault(Basemap_1);
+    var dataUrl = "./data/locations.json";
+    var map = new Map_1.default({
+        ground: {
+            surfaceColor: "#fff"
+        }
+    });
+    function getBaseLayer() {
+        var baseLayer = new VectorTileLayer_1.default({
+            url: "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer"
+        });
+        return esriRequest("./data/basemap-style.json")
+            .then(function (result) {
+            baseLayer.loadStyle(result.data);
+            return baseLayer;
+        });
+    }
+    getBaseLayer()
+        .then(function (baseLayer) {
+        map.basemap = new Basemap_1.default({
+            baseLayers: [baseLayer]
+        });
+    });
+    var view = new SceneView_1.default({
+        map: map,
+        container: "viewDiv",
+        alphaCompositingEnabled: true,
+        camera: {
+            position: {
+                spatialReference: {
+                    wkid: 4326
+                },
+                x: 0.28248677690586,
+                y: 51.553684553226123,
+                z: 25000000
+            },
+            heading: 0,
+            tilt: 0.12089379039103153
+        },
+        environment: {
+            background: {
+                type: "color",
+                color: [0, 0, 0, 0]
+            },
+            lighting: {
+                date: "Thu Nov 15 2018 15:00:00 GMT+0000 (Greenwich MeanTime)"
+            },
+            starsEnabled: false,
+            atmosphereEnabled: false
+        }
+    });
+    view.ui.empty("top-left");
+    // Step 3: Add location points as GeoJSON
+    getData()
+        .then(getGraphics)
+        .then(getLayer)
+        .then(function (layer) {
+        map.add(layer);
+    });
+    function getData() {
+        return esriRequest(dataUrl, {
+            responseType: "json"
+        });
+    }
+    function getGraphics(response) {
+        var geoJson = response.data;
+        return geoJson.features.map(function (feature, i) {
+            return {
+                geometry: new geometry_1.Point({
+                    x: feature.geometry.coordinates[0],
+                    y: feature.geometry.coordinates[1]
+                }),
+                attributes: {
+                    ObjectID: i,
+                    location: feature.properties.location
+                }
+            };
+        });
+    }
+    var fields = [{
+            name: "ObjectID",
+            alias: "ObjectID",
+            type: "oid"
+        },
+        {
+            name: "location",
+            alias: "location",
+            type: "string"
+        }];
+    var colorPalette = ["#FFFFFF", "#F78D99", "#ffc700", "#27004D", "#005892", "#E5211D", "#198E2B", "#f4a142", "#fc8879", "#fcec78", "#8de8b1", "#8dcfe8", "#8d8ee8", "#c88de8"];
+    var uniqueValueInfos = colorPalette.map(function (color, index) {
+        return {
+            value: index,
+            symbol: new symbols_1.PointSymbol3D({
+                symbolLayers: [new symbols_1.ObjectSymbol3DLayer({
+                        material: {
+                            color: color
+                        },
+                        height: 150000,
+                        width: 150000,
+                        resource: { primitive: "sphere" }
+                    })],
+                verticalOffset: {
+                    screenLength: 40,
+                    maxWorldLength: 10000000,
+                    minWorldLength: 10000
+                },
+                callout: {
+                    type: "line",
+                    size: 1.5,
+                    color: "#555"
+                }
+            })
+        };
+    });
+    var renderer = new UniqueValueRenderer_1.default({
+        valueExpression: "$feature.ObjectID % " + colorPalette.length,
+        uniqueValueInfos: uniqueValueInfos
+    });
+    var labelingInfo = [
+        new LabelClass({
+            labelExpressionInfo: { expression: "$feature.location" },
+            symbol: new symbols_1.LabelSymbol3D({
+                symbolLayers: [new symbols_1.TextSymbol3DLayer({
+                        material: { color: "#333" },
+                        size: 10,
+                        font: {
+                            weight: "bold"
+                        },
+                        halo: {
+                            color: "white",
+                            size: 1
+                        }
+                    })]
+            })
+        })
+    ];
+    function getLayer(source) {
+        var layer = new FeatureLayer_1.default({
+            source: source,
+            fields: fields,
+            renderer: renderer,
+            objectIdField: "ObjectID",
+            labelingInfo: labelingInfo
+        });
+        return layer;
+    }
+});
 //# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,182 @@
+import Map from "esri/Map";
+import SceneView from "esri/views/SceneView";
+
+import FeatureLayer from "esri/layers/FeatureLayer";
+import VectorTileLayer from "esri/layers/VectorTileLayer";
+import { Point } from "esri/geometry";
+import UniqueValueRenderer from "esri/renderers/UniqueValueRenderer";
+import { PointSymbol3D, ObjectSymbol3DLayer, LabelSymbol3D, TextSymbol3DLayer } from "esri/symbols";
+import LabelClass = require("esri/layers/support/LabelClass");
+import Basemap from "esri/Basemap";
+
+import esriRequest = require("esri/request");
+import esri = __esri;
+
+const dataUrl = "./data/locations.json";
+
+const map = new Map({
+  ground: {
+    surfaceColor: "#fff"
+  }
+});
+
+function getBaseLayer() {
+
+  const baseLayer = new VectorTileLayer({
+    url: "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer"
+  });
+
+  return esriRequest("./data/basemap-style.json")
+    .then((result) => {
+      baseLayer.loadStyle(result.data);
+      return baseLayer;
+    });
+}
+
+getBaseLayer()
+  .then((baseLayer) => {
+    map.basemap = new Basemap({
+      baseLayers: [baseLayer]
+    });
+  });
+
+const view = new SceneView({
+  map,
+  container: "viewDiv",
+  alphaCompositingEnabled: true,
+  camera: {
+    position: {
+      spatialReference: {
+        wkid: 4326
+      },
+      x: 0.28248677690586,
+      y: 51.553684553226123,
+      z: 25000000
+    },
+    heading: 0,
+    tilt: 0.12089379039103153
+  },
+  environment: {
+    background: {
+      type: "color",
+      color: [0, 0, 0, 0]
+    },
+    lighting: {
+      date: "Thu Nov 15 2018 15:00:00 GMT+0000 (Greenwich MeanTime)"
+    },
+    starsEnabled: false,
+    atmosphereEnabled: false
+  }
+});
+
+view.ui.empty("top-left");
+
+// Step 3: Add location points as GeoJSON
+
+getData()
+  .then(getGraphics)
+  .then(getLayer)
+  .then((layer) => {
+    map.add(layer);
+  });
+
+function getData() {
+  return esriRequest(dataUrl, {
+    responseType: "json"
+  });
+}
+
+function getGraphics(response: esri.RequestResponse) {
+  const geoJson = response.data;
+  return geoJson.features.map((feature: any, i: number) => {
+    return {
+      geometry: new Point({
+        x: feature.geometry.coordinates[0],
+        y: feature.geometry.coordinates[1]
+      }),
+      attributes: {
+        ObjectID: i,
+        location: feature.properties.location
+      }
+    };
+  });
+}
+
+
+const fields = [{
+  name: "ObjectID",
+  alias: "ObjectID",
+  type: "oid"
+},
+{
+  name: "location",
+  alias: "location",
+  type: "string"
+}];
+
+const colorPalette = ["#FFFFFF", "#F78D99", "#ffc700", "#27004D", "#005892", "#E5211D", "#198E2B", "#f4a142", "#fc8879", "#fcec78", "#8de8b1", "#8dcfe8", "#8d8ee8", "#c88de8"];
+
+const uniqueValueInfos = colorPalette.map((color: string, index: number) => {
+  return {
+    value: index,
+    symbol: new PointSymbol3D({
+      symbolLayers: [new ObjectSymbol3DLayer({
+        material: {
+          color: color
+        },
+        height: 150000,
+        width: 150000,
+        resource: { primitive: "sphere" }
+      })],
+      verticalOffset: {
+        screenLength: 40,
+        maxWorldLength: 10000000,
+        minWorldLength: 10000
+      },
+      callout: {
+        type: "line", // autocasts as new LineCallout3D()
+        size: 1.5,
+        color: "#555"
+      }
+    })
+  };
+});
+
+const renderer = new UniqueValueRenderer({
+  valueExpression: `$feature.ObjectID % ${colorPalette.length}`,
+  uniqueValueInfos: uniqueValueInfos
+});
+
+const labelingInfo = [
+  new LabelClass({
+    labelExpressionInfo: { expression: "$feature.location" },
+    symbol: new LabelSymbol3D ({
+      symbolLayers: [ new TextSymbol3DLayer({
+        material: { color: "#333" },
+        size: 10,
+        font: {
+          weight: "bold"
+        },
+        halo: {
+          color: "white",
+          size: 1
+        }
+      })]
+    })
+  })
+];
+
+
+function getLayer(source: esri.Collection<esri.Graphic>) {
+
+  const layer = new FeatureLayer({
+    source,
+    fields,
+    renderer: renderer,
+    objectIdField: "ObjectID",
+    labelingInfo
+  });
+
+  return layer;
+}
+
